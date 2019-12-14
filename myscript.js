b07a1b3cab6d47face6b14cf3c975a7ca18a0ec8
unknown <pangeriz@outlook.com>
2019-12-14 17:58:59 +0000

    new function

--- myscript.js
+++ myscript.js
@@ -1,12 +1,35 @@
-function fizzBuzz() {
+
+/*function fizzBuzz() {
     for (let i = 1; i <= 100; i++) {
-        if (i % 15 == 0)
-            console.log("FizzBuzz");
-        else if (i % 3 == 0)
-            console.log("Fizz");
-        else if (i % 5 == 0)
-            console.log("Buzz");
-        else
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log('FizzBuzz');
+        }
+        else if (i % 3 == 0) {
+            console.log('Fizz');
+        }
+        else if (i % 5 == 0) {
+            console.log('Buzz');
+        }
+        else {
             console.log(i);
+        }
+    }
+}
+fizzBuzz();*/
+
+
+function fizzBuzz() {
+
+    for (let i = 1; i <= 100; i++) {
+
+        if (i % 3 === 0 && i % 5 === 0) {
+            document.getElementById('output').innerHTML += "FizzBuzz<br>";
+        } else if (i % 3 === 0) {
+            document.getElementById('output').innerHTML += "Fizz<br>";
+        } else if (i % 5 === 0) {
+            document.getElementById('output').innerHTML += "Buzz<br>";
+        } else {
+            document.getElementById('output').innerHTML += i + "<br>";
+        }
     }
 }
